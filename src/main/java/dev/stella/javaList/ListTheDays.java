970925c8fa2970af1a82cd2e0f83bfffe95dcd6a
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-02-13 18:37:41 +0100

    test ok

---
+++ src/main/java/dev/stella/javaList/ListTheDays.java
@@ -0,0 +1,63 @@
+package dev.stella.javaList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListTheDays {
+    
+
+    private List<String> dayWheeks;
+    
+
+    public ListTheDays() {
+        this.dayWheeks = new ArrayList<>();
+    }
+
+    public void createListOfDayTheWeek() {
+        dayWheeks.add("Lunes");
+        dayWheeks.add("Martes");
+        dayWheeks.add("Miércoles");
+        dayWheeks.add("Jueves");
+        dayWheeks.add("Viernes");
+        dayWheeks.add("Sábado");
+        dayWheeks.add("Domingo");
+    }
+
+    public List<String> getDayWheeks() {
+      return dayWheeks;
+    }
+ 
+    public int getLenghtList() {
+        return dayWheeks.size();
+    }
+
+    public void deleteDay(String day) {
+        dayWheeks.remove(day);
+    }
+
+    public String getDay(int index) {
+        if (index >= 0 && index < dayWheeks.size()) {
+            return dayWheeks.get(index);
+        
+        
+    }
+        return null;}
+
+    public boolean existDay(String day) {
+        return dayWheeks.contains(day);
+    }
+
+    public void orderAlfabetic() {
+        Collections.sort(dayWheeks);
+    }
+
+    public void clearList() {
+        dayWheeks.clear();
+    }
+
+   
+    public void setDayWheeks(List<String> dayWheeks) {
+        this.dayWheeks = dayWheeks;
+    }
+}
---
+++ src/test/java/dev/stella/javaList/ListTheDaysTests.java
@@ -0,0 +1,47 @@
+package dev.stella.javaList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ListTheDaysTests {
+  
+
+    @Test
+    void testCreateListOfDayTheWeek() {
+        ListTheDays list  = new ListTheDays();
+        list.createListOfDayTheWeek();
+        assertEquals(7 , list.getLenghtList());
+    }
+
+    @Test
+    void testDeleteDay() {
+        ListTheDays list  = new ListTheDays();
+        list.createListOfDayTheWeek();
+        list.deleteDay("Lunes");
+        assertEquals(6 , list.getLenghtList());
+
+    }
+
+
+    @Test
+    void testOrderAlfabetic() {
+        ListTheDays list = new ListTheDays();
+        list.createListOfDayTheWeek();
+        list.orderAlfabetic();
+        assertEquals("Domingo", list.getDay(0));
+        assertEquals("Jueves", list.getDay(1));
+
+    }
+                                            
+   
+    
+    @Test
+    void testClearList() {
+        ListTheDays list = new ListTheDays();
+        list.createListOfDayTheWeek();
+        list.clearList();
+        assertEquals(0, list.getLenghtList());
+
+    }
+}
