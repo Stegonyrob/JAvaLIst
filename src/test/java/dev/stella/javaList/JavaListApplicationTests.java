8dbfaef1d1cd62b1fb74019f183a92bdd5179401
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-02-13 17:54:17 +0100

    first commit

---
+++ src/test/java/dev/stella/javaList/JavaListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stella.javaList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
